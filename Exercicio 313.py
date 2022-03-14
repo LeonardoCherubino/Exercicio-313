9e1c9c282fe2a2f960d5155156d648418da494a7
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2022-03-13 21:44:50 -0300

    Celcius em Fahrenheit

---
+++ Exercicio 313.py
@@ -0,0 +1,6 @@
+#Programa para transformar Celsius Fahrenheit
+
+Celcius = float(input('Entre com o valor da temperatura em Celsius: '))
+f = (9 * Celcius / 5) + 32
+print('O valor de {} graus Celcius e correspondente a {} Fahrenheit'.format(Celcius, f))
+ 
